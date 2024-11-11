f3d44b2aa6f021db9346cc512eaf931096d3cd22
Alexandr Klopovskiy <ocenkapp@yandex.ru>
2024-11-11 12:25:53 +0300

    Изменил порядок тестирования - теперь по кнопке следующий вопрос

--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 import itertools
 import os
-from telegram import Bot
+from telegram import Bot, ReplyKeyboardMarkup
 from telegram.ext import Filters, MessageHandler, PollAnswerHandler, Updater
 from dotenv import load_dotenv
 
@@ -16,6 +16,29 @@ updater = Updater(token=token, use_context=True)
 poll_data: dict = {}
 answers: dict = {}
 wrong_answers: dict = {}
+next_question: dict = {}
+
+
+def start(update, context):
+    if poll_data.get(update.effective_user.id):
+        button = ReplyKeyboardMarkup(
+            [['Продолжить тестирование'],],
+            resize_keyboard=True
+        )
+        return bot.send_message(
+            update.effective_user.id,
+            text='Продолжим тестирование?',
+            reply_markup=button
+        )
+    button = ReplyKeyboardMarkup(
+        [['Начать тестирование'],],
+        resize_keyboard=True
+    )
+    return bot.send_message(
+        update.effective_user.id,
+        text='Привет, начнем тестирование?',
+        reply_markup=button
+    )
 
 
 def generate_polls():
@@ -29,12 +52,17 @@ def generate_polls():
             }
 
 
-def poll(user_id, question, options):
+def poll(user_id, question, options, button_name):
+    button = ReplyKeyboardMarkup(
+        [[button_name],],
+        resize_keyboard=True
+    )
     bot.send_poll(
         user_id,
         question,
         options,
-        is_anonymous=False
+        is_anonymous=False,
+        reply_markup=button
     )
 
 
@@ -58,7 +86,8 @@ def add_wrong_answer(user_id, question, answer):
 def result_message(user_id):
     winning_message = 'Отлично! Все ответы правильные!'
     error_message = (
-        'Есть ошибки! Следует повторить темы с ошибочными ответами.'
+        'Тестирование закончено. Есть ошибки! '
+        'Следует повторить темы с ошибочными ответами.'
     )
     if False not in answers[user_id]:
         bot.send_message(
@@ -129,18 +158,21 @@ def income(update, context):
                 text='Вы уже прошли этот тест!'
             )
         poll_generators = poll_data[update.effective_user.id]
-    else:
+    elif update.message and update.message.text == 'Начать тестирование':
+        next_question[update.effective_user.id] = False
         poll_generator = generate_polls()
         poll_generators = itertools.tee(poll_generator, 2)
         poll_data[update.effective_user.id] = poll_generators
+
     if update.poll_answer:
+        next_question[update.effective_user.id] = True
         prev_item = next(poll_generators[1])
         if update.poll_answer.option_ids[0] + 1 == prev_item['answer']:
             add_answer(
                 update.effective_user.id,
                 True
             )
-            bot.send_message(
+            return bot.send_message(
                 update.effective_user.id,
                 text='Это правильный ответ!'
             )
@@ -154,30 +186,63 @@ def income(update, context):
                 prev_item['question'],
                 prev_item['options'][update.poll_answer.option_ids[0]]
             )
-            bot.send_message(
+            return bot.send_message(
                 update.effective_user.id,
                 text=(
                     'Не правильно! Правильный ответ: '
                     f'{prev_item["options"][prev_item["answer"] - 1]}'
                 )
             )
-    try:
-        question_data = next(poll_generators[0])
-    except StopIteration:
-        result_message(update.effective_user.id)
-        reporting_message(update)
-        return bot.send_message(
-            update.effective_user.id,
-            text='Опрос закончен!'
+    if (
+        update.message
+        and update.message.text in (
+            'Начать тестирование',
+            'Продолжить тестирование'
         )
-    poll(
-        update.effective_user.id,
-        question_data['question'],
-        question_data['options']
-    )
+        and next_question.get(update.effective_user.id) is not None
+        and (
+            next_question[update.effective_user.id] is True
+            or (
+                next_question[update.effective_user.id] is False
+                and update.message.text == 'Начать тестирование'
+            )
+        )
+    ):
+        if poll_data.get(update.effective_user.id):
+            button = 'Продолжить тестирование'
+        else:
+            button = 'Начать тестирование'
+        try:
+            question_data = next(poll_generators[0])
+        except StopIteration:
+            result_message(update.effective_user.id)
+            reporting_message(update)
+            return bot.send_message(
+                update.effective_user.id,
+                text='Опрос закончен!'
+            )
+        poll(
+            update.effective_user.id,
+            question_data['question'],
+            question_data['options'],
+            button
+        )
+        next_question[update.effective_user.id] = False
+    else:
+        bot.send_message(
+            update.effective_user.id,
+            text='Нужно выбрать вариант ответа!'
+        )
 
-
-updater.dispatcher.add_handler(MessageHandler(Filters.text, income))
+updater.dispatcher.add_handler(MessageHandler(
+    Filters.text & Filters.regex('Начать тестирование'),
+    income)
+)
+updater.dispatcher.add_handler(MessageHandler(
+    Filters.text & Filters.regex('Продолжить тестирование'),
+    income)
+)
+updater.dispatcher.add_handler(MessageHandler(Filters.text, start))
 updater.dispatcher.add_handler(MessageHandler(Filters.document, receive_poll))
 updater.dispatcher.add_handler(PollAnswerHandler(income))
 updater.start_polling()
